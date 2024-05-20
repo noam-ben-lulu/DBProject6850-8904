d83d90fa275e202f31a196abfc1cebaab89e5498
noam ben-lulu <128416447+noam-ben-lulu@users.noreply.github.com>
2024-05-20 19:28:18 +0300

    Update CreatTables.sql

--- CreatTables.sql
+++ CreatTables.sql
@@ -1,73 +1,66 @@
-
 CREATE TABLE Positions (
-    PositionsID    NUMBER(38) NOT NULL,
-    PositionsName  VARCHAR2(255) NOT NULL,
+    PositionID     NUMBER(38) NOT NULL,
+    Position_Name  VARCHAR2(255) NOT NULL,
     Requirements   LONG NOT NULL,
-    CONSTRAINT pk_Positions PRIMARY KEY (PositionsID)
-);
-CREATE TABLE Department (
-    DepartmentID   NUMBER(38) NOT NULL,
-    DepartmentName VARCHAR2(255) NOT NULL,
-    Numofworkers   NUMBER(38) NOT NULL,
-    CONSTRAINT pk_Department PRIMARY KEY (DepartmentID)
-);
+    CONSTRAINT pk_Positions PRIMARY KEY (PositionID)
+)
+/
+
+CREATE TABLE department (
+    departmentID   NUMBER(38) NOT NULL,
+    departmentName VARCHAR2(255) NOT NULL,
+    Description    LONG NOT NULL,
+    CONSTRAINT pk_department PRIMARY KEY (departmentID)
+)
+/
 
 CREATE TABLE Employee (
     EmployeeID     NUMBER(38) NOT NULL,
-    FirstName      VARCHAR2(255) NOT NULL,
-    LastName1      VARCHAR2(255) NOT NULL,
-    PositionsID     NUMBER(38) NOT NULL,
-    Salary         NUMBER(38) NOT NULL,
+    PositionID     NUMBER(38) NOT NULL,
+    First_Name     VARCHAR2(255) NOT NULL,
+    Last_Name      VARCHAR2(255) NOT NULL,
     DepartmentID   NUMBER(38) NOT NULL,
+    salary         NUMBER(38) NOT NULL,
     CONSTRAINT pk_Employee PRIMARY KEY (EmployeeID),
-    CONSTRAINT fk_Employee FOREIGN KEY (PositionsID)
-        REFERENCES Positions (PositionsID)
+    CONSTRAINT fk_Employee_Position FOREIGN KEY (PositionID)
+        REFERENCES Positions (PositionID)
         ON DELETE CASCADE,
-    CONSTRAINT fk_Employee2 FOREIGN KEY (DepartmentID)
-        REFERENCES Department (DepartmentID)
+    CONSTRAINT fk_Employee_Department FOREIGN KEY (DepartmentID)
+        REFERENCES department (departmentID)
         ON DELETE CASCADE
-);
-
-CREATE TABLE DateInfo (
-    Day            NUMBER(38) NOT NULL,
-    Month          NUMBER(38) NOT NULL,
-    Year           NUMBER(38) NOT NULL,
-    CONSTRAINT pk_DateInfo PRIMARY KEY (Day, Month, Year)
-);
+)
+/
 
 CREATE TABLE Development (
     DevelopmentID  NUMBER(38) NOT NULL,
-    DepartmentID   NUMBER(38) NOT NULL,
-    InitiativeType VARCHAR2(100) NOT NULL,
-    ImplementationDate DATE NOT NULL,
-    Day            NUMBER(38) NOT NULL,
-    Month          NUMBER(38) NOT NULL,
-    Year           NUMBER(38) NOT NULL,
+    departmentID   NUMBER(38) NOT NULL,
+    initiative_type VARCHAR2(255) NOT NULL,
+    initiative_date DATE NOT NULL,
     CONSTRAINT pk_Development PRIMARY KEY (DevelopmentID),
-    CONSTRAINT fk_Development FOREIGN KEY (DepartmentID)
-        REFERENCES Department (DepartmentID)
-        ON DELETE CASCADE,
-    CONSTRAINT fk_Development2 FOREIGN KEY (Day, Month, Year)
-        REFERENCES DateInfo (Day, Month, Year)
-        ON DELETE CASCADE
-);
+    CONSTRAINT fk_Development FOREIGN KEY (departmentID)
+        REFERENCES department (departmentID)
+)
+/
 
-CREATE TABLE HumanResourceManagement (
+CREATE TABLE Human_Resource_Management (
     HRActionID     NUMBER(38) NOT NULL,
     ActionType     VARCHAR2(255) NOT NULL,
-    DateInfo           DATE NOT NULL,
-    Day            NUMBER(38) NOT NULL,
-    Month          NUMBER(38) NOT NULL,
-    Year           NUMBER(38) NOT NULL,
-    EmployeeID     NUMBER(38) NOT NULL,
-    EmployeeID1    NUMBER(38),
+    HRA_Date       DATE NOT NULL,
     Description    LONG NOT NULL,
-    CONSTRAINT pk_HumanResourceManagement PRIMARY KEY (HRActionID),
-    CONSTRAINT fk_HumanResourceManagement FOREIGN KEY (Day, Month, Year)
-        REFERENCES DateInfo (Day, Month, Year)
-        ON DELETE CASCADE,
-    CONSTRAINT fk_HumanResourceManagement2 FOREIGN KEY (EmployeeID1)
+    EmployeeID     NUMBER(38) NOT NULL,
+    CONSTRAINT pk_Human_Resource_Management PRIMARY KEY (HRActionID),
+    CONSTRAINT fk_Human_Resource_Management FOREIGN KEY (EmployeeID)
         REFERENCES Employee (EmployeeID)
-);
+        ON DELETE CASCADE
+)
+/
 
-
+CREATE TABLE Attendance (
+    EmployeeID     NUMBER(38) NOT NULL,
+    Aten_Date      DATE NOT NULL,
+    Status         VARCHAR2(255) NOT NULL,
+    CONSTRAINT pk_Attendance PRIMARY KEY (EmployeeID, Aten_Date),
+    CONSTRAINT fk_Attendance FOREIGN KEY (EmployeeID)
+        REFERENCES Employee (EmployeeID)
+)
+/
